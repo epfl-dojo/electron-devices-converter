abf89e9b6ce61abbc378a419e961c7a269a89e45
STI-IT <stiitscm@groupes.epfl.ch>
2017-02-10 11:52:12 +0100

    ça marche

--- renderer.js
+++ renderer.js
@@ -1,3 +1,18 @@
 // This file is required by the index.html file and will
 // be executed in the renderer process for that window.
-// All of the Node.js APIs are available in this process.
+// All of the Node.js APIs are available in this process
+window.onload=function(e){
+    init();
+}
+function init() {
+  $( "#inputMonnaie" ).change(function(){
+    var monnaieA = $("input[name='monnaieA']:checked").val();
+    var monnaieD = $("input[name='monnaieD']:checked").val();
+
+    if (monnaieD && monnaieD === monnaieA){
+      $("#result").html($("#inputMonnaie").val())
+    } else {
+      $("#result").html("error")
+    }
+  })
+}
